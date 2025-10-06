9cd77d1ad8ef036215559c39845923b43a4d2470
ayankilje-AK <ayan.kilje@gmail.com>
2025-10-06 19:44:48 +0000

    commit 2

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-print("How old are you")
+name=("AYAN K.")
+Favorite_Subject= ("MATHEMATICS")
+num1=92.35
+num2=10.35
+
+print(" My name is Ayan Kilje, I am from India, but I currently live in Ghana \n" "My favorite subject is Mathematics")
+
+print(num1+num2)
+print("James Webb Telescope", num1+num2+1)
+print("What do you tj=hink, is the answer correct or wrong. If you believe it is wrong, what is the difference between the correct answer and the wrong anser")
+ansinput= ("What is your opinion/ answer")
+if ans == 1:
+    print("Excellent, you are really smart, but lets check if you are the smartest of all time")
+

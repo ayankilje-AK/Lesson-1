091e8494ae32d989908cc32ca78ba40a0a44825e
ayankilje-AK <ayan.kilje@gmail.com>
2025-11-10 19:23:35 +0000

    commit 16

---
+++ operatorprecedence.py
@@ -0,0 +1,15 @@
+a = 12
+b = 3
+c = 7
+d = 6
+e = 0
+e = (a + b) * c/d
+print(e)
+
+f = "Ayan"
+h = 0
+if f == ("Ayan") or f == (" John") and h >= 0:
+    print("Hello, welcome")
+else:
+    ("goodbye")
+    

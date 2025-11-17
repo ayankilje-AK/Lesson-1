46baddc8606bc8a1a550342e37ddd981e65c2954
ayankilje-AK <ayan.kilje@gmail.com>
2025-11-17 19:29:35 +0000

    commit 23

---
+++ stringreverse.py
@@ -0,0 +1,8 @@
+string = input("Kindly enter your own string")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("The original string:", string)
+print("The reversed string:", string2)

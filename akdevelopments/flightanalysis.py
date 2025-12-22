cd53c674cae9cb232f8ba978ad676f1bdaf885ae
ayankilje-AK <ayan.kilje@gmail.com>
2025-12-22 19:41:25 +0000

    commit 41

---
+++ akdevelopments/flightanalysis.py
@@ -0,0 +1,5 @@
+print("Welcome to the AK Flight Features and Demand Analysis Programme Platform")
+GROUPID = input("Please enter your group code: ")
+if GROUPID == ("AB319AK001"):
+    EMPLOYEEID = input("Please enter your employee code: ")
+    if EMPLOYEEID == ("")

d570609f35653b501b12c889416884a9c7faa14d
ayankilje-AK <ayan.kilje@gmail.com>
2026-01-07 19:54:47 +0000

    commit 57

---
+++ radianhw.py
@@ -0,0 +1,9 @@
+import math
+m = float(input("Enter a angle in degrees: "))
+r = math.radians(m)
+sin_value = math.sin(r)
+cos_value = math.cos(r)
+tan_value = math.tan(r)
+print(sin_value)
+print(cos_value)
+print(tan_value)

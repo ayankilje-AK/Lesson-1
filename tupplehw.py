1065cf90696ba0338ef2640575cd6f6a63a64db3
ayankilje-AK <ayan.kilje@gmail.com>
2026-01-21 19:55:57 +0000

    commit 68

---
+++ tupplehw.py
@@ -0,0 +1,13 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+result = 1
+
+for i in tup1:
+    result = result*i
+
+result2 = 1
+for i in tup2:
+    result2 = result2*i
+
+print(result)
+print(result2)

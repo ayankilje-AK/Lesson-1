79db79a855cc2633ed55bfbc6d53526f1ea1b1b5
ayankilje-AK <ayan.kilje@gmail.com>
2026-01-26 19:28:01 +0000

    commit 71

---
+++ set_intersection.py
@@ -0,0 +1,10 @@
+setx = {"Green", "Blue"}
+sety = {"Blue", "Yellow"}
+
+print("original set elements")
+print(setx)
+print(sety)
+
+result = setx.intersection(sety)
+print("\n The intersectional value is: ")
+print(result)

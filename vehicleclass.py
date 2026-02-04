14e710067d9f61c6823028de5e82c768d0e08b6c
ayankilje-AK <ayan.kilje@gmail.com>
2026-02-04 19:44:59 +0000

    commit 82

---
+++ vehicleclass.py
@@ -0,0 +1,10 @@
+class Vehicle:
+
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+obj = Vehicle(240, 18)
+print(obj.max_speed)
+print(obj.mileage)
+

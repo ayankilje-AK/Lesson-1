21734ea39a26d6be941a375d77a3b00eede5d52d
ayankilje-AK <ayan.kilje@gmail.com>
2026-02-11 19:32:40 +0000

    commit 90

---
+++ employee_inheritance.py
@@ -0,0 +1,19 @@
+class Person:
+
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+
+class Employee(Person):
+
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name, idnumber)
+
+obj = Employee("John", 19876543, 20000, "Intern")
+obj.display()
